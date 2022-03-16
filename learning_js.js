176cb529e78ea672af2c011ceb8f5e923b136783
Striwey_F <73765831+Striwey@users.noreply.github.com>
2022-03-16 15:58:43 -0500

    6

--- learning_js.js
+++ learning_js.js
@@ -1,5 +1,5 @@
-var texto = window. prompt("Write the text to evaluate: ");
+var texto = window. prompt("Write the text to evaluate: "), palindromos, aux = "";
 
 for (var i = 0; i < texto.length; i++){
-    
+    aux = aux + 
 }
