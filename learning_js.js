0d4be964dea2b3233d830bc2693a3a4607f599eb
Striwey_F <73765831+Striwey@users.noreply.github.com>
2022-03-28 15:37:05 -0500

    3.1

--- learning_js.js
+++ learning_js.js
@@ -1,6 +1,19 @@
-/*var texto = window. prompt("Write the text to evaluate: "), palindromos, aux = "";
 
-for (var i = 0; i < texto.length; i++){
-    aux = aux + 
-}*/
-window.alert("algun texto");
+var texto = window. prompt("Write the text to evaluate: "), palindromos, aux = "";
+texto = texto.split(" ")
+
+let aux, aux2, lista = [], str_reverse = "";
+
+for (let a = 0; a < texto.length; a++){
+    aux = texto[a]
+    for (let b = 0; b < aux.length; b++){
+        aux2 = aux[b]
+        str_reverse = aux2 + str_reverse
+    }
+    if (str_reverse == aux) {
+        lista.push(str_reverse)
+    }
+}
+
+
+document.getElementById('demo').innerHTML = lista.toString();
