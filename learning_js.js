aedc8046d093a21043076bac3b3510cd6892c7f7
Striwey_F <true.frank.medina@gmail.com>
2022-03-29 22:18:46 -0500

    c.5

--- learning_js.js
+++ learning_js.js
@@ -1,19 +1,20 @@
-let aux, aux2, lista = [], str_reverse = "";
-
-var texto = prompt('Ingrese el texto:');   //comentary
+let aux, aux2, lista = [];
+var texto = prompt('Ingrese el texto:');
 
 texto = texto.split(" ");
 
 for (let a = 0; a < texto.length; a++){
     aux = texto[a]
+    aux = aux.replace(",", "");
 
+    let str_reverse = ""
     for (let b = 0; b < aux.length; b++){
         aux2 = aux[b]
         str_reverse = aux2 + str_reverse
     }
 
     if (str_reverse == aux) {
-        lista.push(str_reverse)
+        lista.push(aux)
     };
 };
 
