acd37c2a81b4c2abd89e0092d38217120e696944
Striwey_F <73765831+Striwey@users.noreply.github.com>
2022-03-29 21:34:09 -0500

    c.1

--- learning_js.js
+++ learning_js.js
@@ -1,17 +1,19 @@
 let aux, aux2, lista = [], str_reverse = "";
-var texto = "ese aviva cualquiera";
-texto = texto.split(" ")
+
+var texto = prompt('Ingrese el texto:');
+
+texto = texto.split(" ");
 
 for (let a = 0; a < texto.length; a++){
     aux = texto[a]
+
     for (let b = 0; b < aux.length; b++){
         aux2 = aux[b]
         str_reverse = aux2 + str_reverse
     }
     if (str_reverse == aux) {
         lista.push(str_reverse)
-    }
-}
-
+    };
+};
 
 document.getElementById('p1').innerHTML = lista.toString();
